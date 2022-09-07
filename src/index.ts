b8700c3aafa7fcfcb76d25f6e2c16234bba7f356
Joliver Estampador <joliverestampador@gmail.com>
2022-09-08 07:56:48 +0800

    adding body parser and router file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import { router } from './routes/loginRoutes';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-    <div>Hi There</div>
-  `);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
