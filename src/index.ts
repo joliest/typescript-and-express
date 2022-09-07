7b8ba846b0dd41ce05ff8a70f1759d842b6b55d5
Joliver Estampador <joliverestampador@gmail.com>
2022-09-08 07:44:06 +0800

    initial express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hi there');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>Hi There</div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
