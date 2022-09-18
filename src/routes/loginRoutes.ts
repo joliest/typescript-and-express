fe48c9a66c219573fd0b949e5c31204ddfe6a38b
Joliver Estampador <joliverestampador@gmail.com>
2022-09-19 07:52:53 +0800

    protecting routes

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,7 +1,17 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send('Not permitted');
 }
 
 const router = Router();
@@ -48,6 +58,15 @@ router.get('/', (req: Request, res: Response) => {
       </div>
     `);
   }
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>Welcome to protected route, logged in user</h1>
+      <a href="/logout">Logout</a>
+    </div>
+  `);
 });
 
 router.get('/logout', (req: Request, res: Response) => {
